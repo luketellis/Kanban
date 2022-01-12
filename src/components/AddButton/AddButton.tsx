be888597bc3aa745d5903641ddade7317dd3ce9c
Luke Tellis <luke.tellis@aroflo.com>
2022-01-12 21:14:22 +1100

    Add button simplification

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import "./addButton.css";
-import { Status } from "../../types/Status";
 
 interface Props {
   onClick: React.MouseEventHandler<HTMLElement>;
-  status: Status;
 }
 
-const addItem = (status: Status): void => {
-  alert(status);
-};
-
-function AddButton({ onClick, status }: Props) {
+function AddButton({ onClick }: Props) {
   return (
     <div className="add-btn" onClick={onClick}>
       <button className="add-span">+ Add Item</button>
